97d415e503d31077fb4c00083cffa27dff06d3f8
HasibGit <hasibullah04@gmail.com>
2023-09-18 22:55:22 +0600

    implemented displaying category data on view

--- MakyrWeb/Controllers/CategoryController.cs
+++ MakyrWeb/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MakyrWeb.Data;
+using MakyrWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MakyrWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
         }
     }
 }
--- MakyrWeb/Data/ApplicationDbContext.cs
+++ MakyrWeb/Data/ApplicationDbContext.cs
@@ -5,10 +5,20 @@ namespace MakyrWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+        }
     }
 }
