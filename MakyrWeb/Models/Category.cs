c032869fe89b72bb26d3536409f5314e720dbf32
HasibGit <hasibullah04@gmail.com>
2023-09-22 21:52:58 +0600

    added server side validation

--- MakyrWeb/Models/Category.cs
+++ MakyrWeb/Models/Category.cs
@@ -10,9 +10,11 @@ namespace MakyrWeb.Models
 
         [Required]
         [DisplayName("Category Name")]
+        [Range(3, 50)]
         public string Name { get; set; }
 
         [Required]
+        [Range(1, 100, ErrorMessage = "Display order must be within 1-100")]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
