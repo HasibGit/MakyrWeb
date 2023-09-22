f050854b996255b495c95198745cc96d5cc13755
HasibGit <hasibullah04@gmail.com>
2023-09-22 20:15:41 +0600

    form control property binding done with category model

--- MakyrWeb/Models/Category.cs
+++ MakyrWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MakyrWeb.Models
 {
@@ -6,9 +7,13 @@ namespace MakyrWeb.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
         [Required]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
