281a41afef1a6e6c552b6105da74cd7fb314bdbd
Hasib Ullah <hasibullah04@gmail.com>
2023-09-30 03:02:17 +0600

    added client side validation

--- MakyrWeb/Program.cs
+++ MakyrWeb/Program.cs
@@ -6,10 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-        )
-    );
+    options =>
+    {
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection"),
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 5,  // Maximum number of retries
+                maxRetryDelay: TimeSpan.FromSeconds(30), // Delay between retries
+                errorNumbersToAdd: null);
+            }
+            );
+    });
 
 var app = builder.Build();
 
