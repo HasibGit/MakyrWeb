5fab64d54112b4c7ab0c5cbca3b7472fe7a73191
HasibGit <hasibullah04@gmail.com>
2023-10-02 02:06:04 +0600

    working on edit functionality

--- MakyrWeb/Controllers/CategoryController.cs
+++ MakyrWeb/Controllers/CategoryController.cs
@@ -33,5 +33,33 @@ namespace MakyrWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
