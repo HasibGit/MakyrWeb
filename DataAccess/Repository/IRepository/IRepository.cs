0791ee956a995c790b3e6da447900ebd6c56af86
HasibGit <hasibullah04@gmail.com>
2023-10-16 22:27:32 +0600

    repository interface implemented

---
+++ DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makyr.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T Get(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        Task RemoveAsync(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        
+
+    }
+}
---
+++ DataAccess/Repository/IRepository/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makyr.DataAccess.Repository.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();  
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            return dbSet.SingleOrDefault(filter);
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbSet.SingleOrDefaultAsync(filter);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
