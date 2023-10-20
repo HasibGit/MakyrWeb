f92ee3a128e73a6923a62de6a7acb60873d41496
HasibGit <hasibullah04@gmail.com>
2023-10-20 23:59:06 +0600

    Seeded gpu dummy data

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Makyr.DataAccess
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<GraphicsCard> GraphicsCards { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,93 @@ namespace Makyr.DataAccess
                 new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category() { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
                 new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+
+            modelBuilder.Entity<GraphicsCard>().HasData(
+                new GraphicsCard()
+                {
+                    Id = Guid.NewGuid(),
+                    Brand = "Nvidia",
+                    Model = "GTX 1080",
+                    ProductCode = "ABC123",
+                    Status = "New",
+                    Price = 599.99,
+                    MemoryType = "GDDR5",
+                    MemorySize = 8,
+                    BaseClock = 1607,
+                    BoostClock = 1733,
+                    MemoryClockSpeed = 10,
+                    BusType = 256,
+                    CudaCores = 2560,
+                    DisplayPort = "3",
+                    HdmiPort = "1",
+                    Connectors = "8-pin",
+                    RecommendedPsu = 500,
+                    Directx = "DirectX 12",
+                    OpenGL = "OpenGL 4.5",
+                    Width = 267.7,
+                    Height = 111.2,
+                    Depth = 38.1,
+                    Condition = "New",
+                    ManufacturingWarrenty = 12,
+                    ServiceWarrenty = 0
+                },
+                new GraphicsCard()
+                {
+                    Id = Guid.NewGuid(),
+                    Brand = "AMD",
+                    Model = "RX 5700 XT",
+                    ProductCode = "XYZ456",
+                    Status = "Used",
+                    Price = 299.99,
+                    MemoryType = "GDDR6",
+                    MemorySize = 8,
+                    BaseClock = 1605,
+                    BoostClock = 1905,
+                    MemoryClockSpeed = 14,
+                    BusType = 256,
+                    CudaCores = 0,  // Not applicable to AMD cards
+                    DisplayPort = "3",
+                    HdmiPort = "1",
+                    Connectors = "8-pin",
+                    RecommendedPsu = 600,
+                    Directx = "DirectX 12",
+                    OpenGL = "OpenGL 4.6",
+                    Width = 268,
+                    Height = 116.6,
+                    Depth = 40,
+                    Condition = "Used",
+                    ManufacturingWarrenty = 0,
+                    ServiceWarrenty = 6
+                },
+                new GraphicsCard()
+                {
+                    Id = Guid.NewGuid(),
+                    Brand = "MSI",
+                    Model = "RTX 3060 Ti",
+                    ProductCode = "LMN789",
+                    Status = "New",
+                    Price = 449.99,
+                    MemoryType = "GDDR6",
+                    MemorySize = 8,
+                    BaseClock = 1410,
+                    BoostClock = 1695,
+                    MemoryClockSpeed = 14,
+                    BusType = 256,
+                    CudaCores = 4864,
+                    DisplayPort = "3",
+                    HdmiPort = "1",
+                    Connectors = "8-pin",
+                    RecommendedPsu = 650,
+                    Directx = "DirectX 12",
+                    OpenGL = "OpenGL 4.6",
+                    Width = 245,
+                    Height = 115,
+                    Depth = 39,
+                    Condition = "New",
+                    ManufacturingWarrenty = 24,
+                    ServiceWarrenty = 0
+                }
             );
         }
     }
---
+++ DataAccess/Migrations/20231020175637_AddGraphicCardTableToDb.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Makyr.DataAccess.Migrations
+{
+    public partial class AddGraphicCardTableToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "GraphicsCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ProductCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    MemoryType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemorySize = table.Column<int>(type: "int", nullable: false),
+                    BaseClock = table.Column<int>(type: "int", nullable: false),
+                    BoostClock = table.Column<int>(type: "int", nullable: false),
+                    MemoryClockSpeed = table.Column<int>(type: "int", nullable: false),
+                    BusType = table.Column<int>(type: "int", nullable: false),
+                    CudaCores = table.Column<int>(type: "int", nullable: false),
+                    DisplayPort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HdmiPort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Connectors = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecommendedPsu = table.Column<int>(type: "int", nullable: false),
+                    Directx = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpenGL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Width = table.Column<double>(type: "float", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false),
+                    Depth = table.Column<double>(type: "float", nullable: false),
+                    Condition = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManufacturingWarrenty = table.Column<int>(type: "int", nullable: false),
+                    ServiceWarrenty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GraphicsCards", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "GraphicsCards",
+                columns: new[] { "Id", "BaseClock", "BoostClock", "Brand", "BusType", "Condition", "Connectors", "CudaCores", "Depth", "Directx", "DisplayPort", "HdmiPort", "Height", "ManufacturingWarrenty", "MemoryClockSpeed", "MemorySize", "MemoryType", "Model", "OpenGL", "Price", "ProductCode", "RecommendedPsu", "ServiceWarrenty", "Status", "Width" },
+                values: new object[] { new Guid("2ed3ff50-a9de-496b-ba3f-fccc775ef9e9"), 1605, 1905, "AMD", 256, "Used", "8-pin", 0, 40.0, "DirectX 12", "3", "1", 116.59999999999999, 0, 14, 8, "GDDR6", "RX 5700 XT", "OpenGL 4.6", 299.99000000000001, "XYZ456", 600, 6, "Used", 268.0 });
+
+            migrationBuilder.InsertData(
+                table: "GraphicsCards",
+                columns: new[] { "Id", "BaseClock", "BoostClock", "Brand", "BusType", "Condition", "Connectors", "CudaCores", "Depth", "Directx", "DisplayPort", "HdmiPort", "Height", "ManufacturingWarrenty", "MemoryClockSpeed", "MemorySize", "MemoryType", "Model", "OpenGL", "Price", "ProductCode", "RecommendedPsu", "ServiceWarrenty", "Status", "Width" },
+                values: new object[] { new Guid("873ff6c5-b2fe-4dd2-a6be-64fc86d51941"), 1607, 1733, "Nvidia", 256, "New", "8-pin", 2560, 38.100000000000001, "DirectX 12", "3", "1", 111.2, 12, 10, 8, "GDDR5", "GTX 1080", "OpenGL 4.5", 599.99000000000001, "ABC123", 500, 0, "New", 267.69999999999999 });
+
+            migrationBuilder.InsertData(
+                table: "GraphicsCards",
+                columns: new[] { "Id", "BaseClock", "BoostClock", "Brand", "BusType", "Condition", "Connectors", "CudaCores", "Depth", "Directx", "DisplayPort", "HdmiPort", "Height", "ManufacturingWarrenty", "MemoryClockSpeed", "MemorySize", "MemoryType", "Model", "OpenGL", "Price", "ProductCode", "RecommendedPsu", "ServiceWarrenty", "Status", "Width" },
+                values: new object[] { new Guid("d3dea808-0654-4e6d-89c7-2e5c1efc113e"), 1410, 1695, "MSI", 256, "New", "8-pin", 4864, 39.0, "DirectX 12", "3", "1", 115.0, 24, 14, 8, "GDDR6", "RTX 3060 Ti", "OpenGL 4.6", 449.99000000000001, "LMN789", 650, 0, "New", 245.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GraphicsCards");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
